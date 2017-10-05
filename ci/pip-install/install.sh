12884d0e00f886874ef9061c38cb86c9f6b61add
David W.H. Swenson <dwhs@hyperblazer.net>
2017-10-05 11:32:43 +0200

    Explicit builds in pip-install/install.sh

--- ci/pip-install/install.sh
+++ ci/pip-install/install.sh
@@ -1,4 +1,8 @@
 pip install --upgrade pip
+pip install cython
 pip install --upgrade --force-reinstall numpy  # override Travis numpy
+pip install scipy
+pip install pandas
+pip install mdtraj
 pip install -r requirements.txt
 pip install .
