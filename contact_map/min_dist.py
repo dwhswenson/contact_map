82609f66c9ae4539426694970088dff588a47a5d
David W.H. Swenson <dwhs@hyperblazer.net>
2017-10-16 14:17:21 +0200

    Major updates to NearestAtoms; plus testing
    First test for NearestAtoms. Also, update PDB to be useful for
    min_dist-related tests.

--- contact_map/min_dist.py
+++ contact_map/min_dist.py
@@ -4,22 +4,73 @@ import mdtraj as md
 
 class NearestAtoms(object):
     """
+    Identify nearest atoms (within a cutoff) to an atom.
+
+    Parameters
+    ----------
+    trajectory : :class:`mdtraj.Trajectory`
+        trajectory to be analyzed
+    cutoff : float
+        cutoff distance (in nm)
+    frame_number : int
+        frame number within the trajectory (counting from 0), default 0
+    excluded : dict
+        a dict of {atom_index: [excluded_atom_indices]}, where the excluded
+        atom indices are atoms that should not be counted when considering
+        the atom for the key atom_index. Default is ``None``, which ignores
+        all atoms in the same residue. Passing an empty dict, ``{}``, will
+        result in all atom pairs being considered
     """
-    def __init__(self, trajectory, cutoff, frame_number=0):
-        # TODO: add support for a subset of all atoms with `atoms`
+    # TODO: this can probably be refactored to match the behavior of the
+    # mindist object; can't be fully removed because this will be a more
+    # expensive calc
+    def __init__(self, trajectory, cutoff, frame_number=0, excluded=None):
         self.cutoff = cutoff
         self.frame_number = frame_number
-        neighborlist = md.compute_neighborlist(trajectory, self.cutoff,
-                                               self.frame_number)
-        frame = trajectory[frame_number]
-        self.nearest = {}
-        self.nearest_distance = {}
+        self.excluded = self._parse_excluded(excluded, trajectory)
+        self.nearest, self.nearest_distance = \
+                self._calculate_nearest(trajectory)
+
+
+    def _calculate_nearest(self, trajectory, cutoff=None, frame_number=None,
+                           excluded=None):
+        if cutoff is None:
+            cutoff = self.cutoff
+        if excluded is None:
+            excluded = self.excluded
+        if frame_number is None:
+            frame_number = self.frame_number
+
+
+        neighborlist = md.compute_neighborlist(trajectory, cutoff,
+                                               frame_number)
+        nearest = {}
+        nearest_distance = {}
         for (atom, neighbors) in enumerate(neighborlist):
-            pairs = itertools.product([atom], neighbors)
-            distances = md.compute_distances(frame, pairs)[0]  # 0th frame
-            nearest = sorted(zip(distances, neighbors))[0]
-            self.nearest[atom] = nearest[1]
-            self.nearest_distance[atom] = nearest[0]
+            allowed_neighbors = [n for n in neighbors
+                                 if n not in excluded[atom]]
+            pairs = list(itertools.product([atom], allowed_neighbors))
+            if len(pairs) != 0:
+                distances = md.compute_distances(trajectory,
+                                                 pairs)[frame_number]
+
+                nearest_tuple = \
+                        sorted(list(zip(distances, allowed_neighbors)))[0]
+                nearest[atom] = nearest_tuple[1]
+                nearest_distance[atom] = nearest_tuple[0]
+        return (nearest, nearest_distance)
+
+    @staticmethod
+    def _parse_excluded(excluded, trajectory):
+        if excluded == {}:
+            excluded = {idx: [] for idx in range(trajectory.n_atoms)}
+        if excluded is None:
+            top = trajectory.topology
+            excluded = {
+                idx: [a.index for a in top.atom(idx).residue.atoms]
+                for idx in range(trajectory.n_atoms)
+            }
+        return excluded
 
     @property
     def sorted_distances(self):
@@ -27,22 +78,36 @@ class NearestAtoms(object):
                   for (atom, dist) in list(self.nearest_distance.items())]
         return sorted(listed, key=lambda tup: tup[2])
 
+
 class MinimumDistanceCounter(object):
+    """Count how often each atom pair is the minimum distance.
+    """
     # count how many times each atom pair has minimum distance
     def __init__(self, trajectory, query, haystack):
-        self.contact_map = None
-        self.atom_pairs = list(itertools.product(query, haystack))
-        distances = md.compute_distances(trajectory,
-                                         atom_pairs=self.atom_pairs)
-        self._min_pairs = distances.argmin(axis=1)
-        self.minimum_distances = distances.min(axis=1)
+        # TODO: better approach is to make the initialization based on
+        # atom_pairs
         self.topology = trajectory.topology
-        self.cutoff = cutoff
+        self.atom_pairs = list(itertools.product(query, haystack))
+        self.minimum_distances, self._min_pairs = \
+                self._compute_from_atom_pairs(trajectory, self.atom_pairs)
 
     @classmethod
-    def from_contact_map(cls, contact_map):
-        # TODO: create query and haystack from actual contacts
-        pass
+    def from_contact_map(cls, trajectory, contact_map):
+        """contact_map based constructor"""
+        min_dist = cls.__new__()
+        min_dist.topology = trajectory.topology
+        min_dist.atom_pairs = [list(c) for c in contact_map.atom_contacts]
+        min_dist.minimum_distances, min_dist._min_pairs = \
+                min_dist._compute_from_atom_pairs(trajectory,
+                                                  min_dist.atom_pairs)
+        return min_dist
+
+    @staticmethod
+    def _compute_from_atom_pairs(trajectory, atom_pairs):
+        distances = md.compute_distances(trajectory, atom_pairs)
+        min_pairs = distances.argmin(axis=1)
+        minimum_distances = distances.min(axis=1)
+        return minimum_distances, min_pairs
 
     def _remap(self, pair_number):
         pair = self.atom_pairs[pair_number]
--- contact_map/tests/test_min_dist.py
+++ contact_map/tests/test_min_dist.py
@@ -1,8 +1,9 @@
-import mdtraj as md
+# pylint: disable=wildcard-import, unused-wildcard-import
+# pylint: disable=wrong-import-order
+# pylint: disable=missing-docstring, no-self-use, protected-access
+# pylint: disable=attribute-defined-outside-init, invalid-name
 
-# pylint: disable=wildcard-import, missing-docstring, protected-access
-# pylint: disable=attribute-defined-outside-init, invalid-name, no-self-use
-# pylint: disable=wrong-import-order, unused-wildcard-import
+import mdtraj as md
 
 #includes pytest
 from .utils import *
@@ -11,14 +12,39 @@ from contact_map.min_dist import *
 
 traj = md.load(find_testfile("trajectory.pdb"))
 
+@pytest.mark.parametrize("idx", [0, 4])
 class TestNearestAtoms(object):
     def setup(self):
-        pass
+        self.nearest_atoms = {
+            idx: NearestAtoms(traj, cutoff=0.075, frame_number=idx)
+            for idx in [0, 4]
+        }
 
-    def test_initialization(self):
-        pytest.skip()
+    def test_initialization(self, idx):
+        nearest = self.nearest_atoms[idx]
+        expected_nearest_atoms = {
+            0: {1: 4, 4: 6, 5: 6, 6: 5},
+            4: {0: 9, 1: 8, 4: 6, 5: 7, 6: 4, 7: 5, 8: 1, 9: 0}
+        }
+        expected_nearest_distance = {
+            0: {1: 0.070710678118654752440,  # 0.1*0.5*sqrt(2)
+                4: 0.063639610306789277196,  # 0.1*sqrt(0.45^2 + 0.45^2)
+                5: 0.045276925690687083132,  # 0.1*sqrt(0.05^2 + 0.45^2)
+                6: 0.045276925690687083132},
+            4: {0: 0.045,
+                1: 0.045,
+                4: 0.05,
+                5: 0.05,
+                6: 0.05,
+                7: 0.05,
+                8: 0.045,
+                9: 0.045}
+        }
+        assert nearest.nearest == expected_nearest_atoms[idx]
+        expected_distance = pytest.approx(expected_nearest_distance[idx])
+        assert nearest.nearest_distance == expected_distance
 
-    def test_sorted_distances(self):
+    def test_sorted_distances(self, idx):
         pytest.skip()
 
 class TestMinimumDistanceCounter(object):
