e9b9d1684eb8fa4ded362d3e2b494c80085ac3f2
David W.H. Swenson <dwhs@hyperblazer.net>
2018-01-20 13:12:39 +0100

    Skeleton of tests for frequency_task

---
+++ contact_map/tests/test_frequency_task.py
@@ -0,0 +1,44 @@
+import os
+import collections
+
+from .utils import *
+from .test_contact_map import traj
+
+from contact_map.frequency_task import *
+
+
+class TestSlicing(object):
+    # tests for block_slices and default_slices
+    def test_block_slices_even_split(self):
+        pytest.skip()
+
+    def test_block_slices_remainder(self):
+        pytest.skip()
+
+    def test_default_slice_even_split(self):
+        pytest.skip()
+
+    def test_default_slice_remainder(self):
+        pytest.skip()
+
+class TestTasks(object):
+    def test_load_trajectory_task(self):
+        pytest.skip()
+
+    def test_map_task(self):
+        pytest.skip()
+
+    def test_reduce_task(self):
+        pytest.skip()
+
+    def test_map_task_json(self):
+        pytest.skip()
+
+    def test_reduce_all_results_json(self):
+        pytest.skip()
+
+    def test_integration_object_based(self):
+        pytest.skip()
+
+    def test_intergration_json_based(self):
+        pytest.skip()
