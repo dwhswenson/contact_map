ed5dc2f4a9e7ba3007dfe4e33f418ae76fdfdb1f
David W.H. Swenson <dwhs@hyperblazer.net>
2019-03-19 13:08:39 +0100

    more attempts to fix py37 builds

--- ci/pip-install/install_requirements.sh
+++ ci/pip-install/install_requirements.sh
@@ -4,4 +4,4 @@ pip install --upgrade pip
 pip install cython  # may be required for numpy override?
 pip install --upgrade --force-reinstall numpy  # override Travis numpy
 # --no-binary required until MDTraj updates its wheels
-pip install --no-binary -r requirements.txt
+pip install -r requirements.txt
