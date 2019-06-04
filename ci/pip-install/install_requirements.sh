e4cc55cad49015f815318572c5e919988717f796
David W.H. Swenson <dwhs@hyperblazer.net>
2019-06-04 19:07:37 +0200

    cleanup

--- ci/pip-install/install_requirements.sh
+++ ci/pip-install/install_requirements.sh
@@ -3,5 +3,4 @@
 pip install --upgrade pip
 pip install cython  # may be required for numpy override?
 pip install --upgrade --force-reinstall numpy  # override Travis numpy
-# --no-binary required until MDTraj updates its wheels
 pip install -r requirements.txt
