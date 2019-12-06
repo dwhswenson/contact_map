acc93507071ef087c1e26b8c7cdb24acbac32d88
David W.H. Swenson <dwhs@hyperblazer.net>
2019-12-06 14:11:39 +0100

    update autorelease_check for autorelease 0.1.0

--- autorelease_check.py
+++ autorelease_check.py
@@ -3,17 +3,19 @@ from __future__ import print_function
 import setup
 import contact_map
 from autorelease import DefaultCheckRunner, conda_recipe_version
+from autorelease.version import get_setup_version
 from packaging.version import Version
 
 repo_path = '.'
+SETUP_VERSION = get_setup_version(None, directory='.')
 versions = {
     'package': contact_map.version.version,
-    'setup.py': setup.PACKAGE_VERSION,
+    'setup.py': SETUP_VERSION,
     'conda-recipe': conda_recipe_version('ci/conda-recipe/meta.yaml'),
 }
 
 RELEASE_BRANCHES = ['stable']
-RELEASE_TAG = "v" + Version(setup.PACKAGE_VERSION).base_version
+RELEASE_TAG = "v" + Version(SETUP_VERSION).base_version
 
 if __name__ == "__main__":
     checker = DefaultCheckRunner(
