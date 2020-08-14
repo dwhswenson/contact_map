638b2dd41ec9dca4d8a79f1ec2da95683ad10a40
David W.H. Swenson <dwhs@hyperblazer.net>
2020-08-14 17:13:23 +0200

    Add atom_indexer!

---
+++ contact_map/atom_indexer.py
@@ -0,0 +1,44 @@
+import collections
+
+class AtomSlicedIndexer(object):
+    # __slots__ = ['all_atoms', 'sliced_idx', 'real_idx', 'query', 'haystack',
+                 # 'atom_idx_to_residue_idx']
+    def __init__(self, topology, real_query, real_haystack, all_atoms):
+        self.sliced_idx = {
+            real_idx : sliced_idx
+            for sliced_idx, real_idx in enumerate(all_atoms)
+        }
+        self.real_idx = {
+            sliced_idx: real_idx
+            for real_idx, sliced_idx in self.sliced_idx.items()
+        }
+        self.query = set([self.sliced_idx[q] for q in real_query])
+        self.haystack = set([self.sliced_idx[h] for h in real_haystack])
+
+        real_atom_idx_to_residue_idx = {atom.index: atom.residue.index
+                                        for atom in topology.atoms}
+        self.atom_idx_to_residue_idx = {
+            sliced_idx: real_atom_idx_to_residue_idx[real_idx]
+            for sliced_idx, real_idx in enumerate(all_atoms)
+        }
+
+    def convert_atom_contacts(self, atom_contacts):
+        result =  {frozenset(self.real_idx[a] for a in key): value
+                   for key, value in atom_contacts.items()}
+        return collections.Counter(result)
+
+
+class IdentityIndexer(object):
+    # __slots__ = ['all_atoms', 'sliced_idx', 'real_idx', 'query', 'haystack',
+                 # 'atom_idx_to_residue_idx']
+    def __init__(self, topology, real_query, real_haystack, all_atoms):
+        identity_mapping = {a: a for a in range(topology.n_atoms)}
+        self.sliced_idx = identity_mapping
+        self.real_idx = identity_mapping
+        self.query = set(real_query)
+        self.haystack = set(real_haystack)
+        self.atom_idx_to_residue_idx = {atom.index: atom.residue.index
+                                        for atom in topology.atoms}
+
+    def convert_atom_contacts(self, atom_contacts):
+        return atom_contacts
